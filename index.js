38a8fe58dfd3a086d2a3c40194fa5cd57c2a3c9b
DestinyFrog <107517847+DestinyFrog@users.noreply.github.com>
2024-09-08 12:25:48 -0300

    auto.js to index.js

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import File from './file.js'
+import Acidos from './tipos/acidos.js'
 import Bases from './tipos/bases.js'
 
 File.Upload()
 
-const data = Bases.Update()
+const data = [
+	... Bases.Update(),
+	... Acidos.Update()
+]
 
 File.Download(data)
